d6b1edbdbc7714a0de68f7bbeffe4a983ae2d245
kagemune.yd <kagemune.yd@gmail.com>
2025-08-01 08:05:46 -0500

    apliacion terminada

--- cardgenerator.py
+++ cardgenerator.py
@@ -1,8 +1,7 @@
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 from html2image import Html2Image
-from PIL import Image
-import time
+
 # from weasyprint import HTML
 import os
 # 1. Crear rutas absolutas y normalizadas
@@ -42,20 +41,6 @@ template_path = 'tarjetasEmpleados1.html'
 output_folder = './tarjetas/'
 os.makedirs(output_folder, exist_ok=True)
 
-# def verificar_permisos(ruta):
-#     try:
-#         test_file = os.path.join(ruta, 'test.txt')
-#         with open(test_file, 'w') as f:
-#             f.write('test')
-#         os.remove(test_file)
-#         return True
-#     except:
-#         return False
-
-# if not verificar_permisos(temp_dir):
-#     temp_dir = os.path.normpath("C:/temp/html2image")
-#     os.makedirs(temp_dir, exist_ok=True, mode=0o777)
-
 # Cargar datos
 df = pd.read_csv(csv_path)
 # Verificar que el archivo CSV se cargó correctamente
@@ -88,41 +73,19 @@ def generar_imagen_segura(hti, html_content, output_filename):
     except Exception as e:
         print(f"Error al generar {output_filename}: {str(e)}")
         return False
-def cortar_imagen(input_path, output_folder, size=(328, 208)):
-    """Corta la imagen a un tamaño específico."""
-    try:
-        with Image.open(input_path) as img:
-            img = img.resize(size, Image.ANTIALIAS)
-            img.save(output_folder)
-        return True
-    except Exception as e:
-        print(f"Error al cortar imagen {input_path}: {str(e)}")
-        return False   
-
+    
+    
+# Procesar cada empleado y generar imágenes
 for _, empleado in df.iterrows():
     html_output = template.render(**empleado.to_dict())
     output_image = f"tarjeta_{empleado['Cédula']}{empleado['Nombres']}{empleado['Apellidos']}.png"
     
     if generar_imagen_segura(hti, html_output, output_image):
-        # Cortar la imagen a un tamaño específico
-       
-        if cortar_imagen(output_image, output_folder + output_image):
-            print(f"✓ Imagen cortada: {output_image}")
         print(f"✓ Imagen generada: {output_image}")
     else:
         print(f"✗ Falló generación para empleado {empleado['Cédula']}")
 
-# for _, empleado in df.iterrows():
-    # try:
-        # html_output = template.render(**empleado.to_dict())
-        # output_image = os.path.join(output_dir, f"tarjeta_{empleado['Cédula']}.png")
-        # 
-        # hti.screenshot(
-            # html_str=html_output,
-            # save_as=output_image
-        # )
-        # print(f"Imagen generada: {output_image}")
-    # except Exception as e:
-        # print(f"Error con empleado {empleado['Cédula']}: {str(e)}")
+# Finalizar
+hti.close()  # Cerrar Html2Image para liberar recursos
 
 print("Proceso completado!")
